6dd5ba0bc493e924d762b82249809bb9593dc580
Keith Atienza <keithatienza19@gmail.com>
2017-10-12 23:05:28 -0700

    added comments and rearranged functions

--- homework1/homework1/Cards.cpp
+++ homework1/homework1/Cards.cpp
@@ -3,28 +3,19 @@
 #include <iostream>
 #include <time.h>
 #include <Windows.h>
-
-/*
-You might or might not need these two extra libraries
 #include <iomanip>
-#include <algorithm>
-*/
-
 
 /* *************************************************
 Card class
 ************************************************* */
 
-/*
-Default constructor for the Card class.
-It could give repeated cards. This is OK.
-Most variations of Blackjack are played with
-several decks of cards at the same time.
-*/
-Card::Card() {
-	srand(time(0));
+// Constructor assigns random rank & suit to card.
 
-	int r = 1 + rand() % 3;
+Card::Card() {
+	srand(time(0)); // seed 
+
+	// time delay is added so that random cards would be drawn by the computer (ie. not on the same second)
+	int r = 1 + rand() % 3; 
 
 	if (r == 1)
 	{
@@ -189,6 +180,7 @@ string Card::get_english_rank() const
 	return rankName;
 }
 
+// returns the corresponding values of the cards 
 double Card::get_value() const
 {
 	double value = 0;
@@ -248,15 +240,20 @@ bool Card::operator < (Card card2) const {
 Player class
 ************************************************* */
 
+// constructors, one with initial money
 Player::Player() : myhand({}), money(0) {}
 
 Player::Player(int m) : myhand({}), money(m) {}
 
+/**
+	computes the total of the value of the cards
+	@return value of the card
+*/
 double Player::card_total() const
 {
 
 	double total = 0;
-	for (const auto& x : myhand)
+	for (const auto& x : myhand) // add every value of card to total
 	{
 		total += x->get_value();
 	}
@@ -264,15 +261,20 @@ double Player::card_total() const
 
 }
 
-
+/**
+	adds another card into the hand by pushing back a Card*
+*/
 void Player::draw_card()
 {
 	Card* newcard = new Card(); 
 	myhand.push_back(newcard);
-	//cout << "\t" << newcard->get_spanish_rank() << " de " << newcard->get_spanish_suit() << "\t ("
-		//<< newcard->get_english_rank() << " of " << newcard->get_english_suit() << ")" << endl;
+	return;
 }
 
+/**
+	checks if player loses the round (ie. card total is more than 7.5)
+	@return true if lost, false if not
+*/
 bool Player::check_lose() const
 {
 	if (card_total() > 7.5)
@@ -281,6 +283,10 @@ bool Player::check_lose() const
 		return false;
 }
 
+/**
+	checks if the game has reached its end (ie. money == 0)
+	@return true if game is finished, false if not
+*/
 bool Player::check_end() const
 {
 	if (money == 0)
@@ -289,122 +295,253 @@ bool Player::check_end() const
 		return false;
 }
 
-bool Player::operator < (Player another) const
+/**
+	compares who won between the two players based on the total value of their cards
+	@param another the player with whom to compare
+	@return true if rhs player has a total higher than the lhs player but lower than 7.5
+*/
+bool Player::operator < (const Player& another) const
 {
 	return (card_total() < another.card_total());
 }
 
+/**
+	accessor for money
+	@return the value of money the player has currently
+*/
 int Player::get_money() const
 {
 	return money;
 }
-void Player::win_bet(int a)
+
+
+/**
+	sets money of the player
+	@param money to be set
+*/
+void Player::set_money(const int& a)
+{
+	money = a;
+	return;
+}
+
+/**
+	adds bet to the money
+	@param a amount of bet won
+*/
+void Player::win_bet(const int& a)
 {
 	money += a;
+	return;
 }
-void Player::lose_bet(int b)
+
+/**
+	subtracts bet from money
+	@param b amount of money lost
+*/
+void Player::lose_bet(const int& b)
 {
 	money -= b;
+	return;
 }
 
+/**
+	list cards into the console using cout
+*/
 void Player::list_cards() const // list cards in their names in spanish and english
 {
-	for (const auto& x : myhand)
+	string line_up = "";
+
+	for (const auto& x : myhand) // for every card, cout
 	{
-		cout << "\t" << x->get_spanish_rank() << " de " << x->get_spanish_suit() << "\t (" 
-			<< x->get_english_rank() << " of " << x->get_english_suit() << ")" << endl;
+		line_up = x->get_spanish_rank() + " de " + x->get_spanish_suit(); // i want the setw to apply to this whole string
+
+		cout << "\t" << left << setw(20) << line_up << "(" << x->get_english_rank() << " of " << x->get_english_suit() << ")" << endl;
 	}
+	return;
 }
 
+/**
+	list cards into an ostream object to be used when logging into a txt file
+	@param ost ostream object to write into the txt file
+*/
 void Player::log_cards(ostream& ost) const
 {
-	for (const auto& x : myhand)
+	string line_up = "";
+
+	for (const auto& x : myhand) // for every card, put into ost
 	{
-		ost << "\t" << x->get_spanish_rank() << " de " << x->get_spanish_suit() << "\t ("
-			<< x->get_english_rank() << " of " << x->get_english_suit() << ")" << endl;
+		line_up = x->get_spanish_rank() + " de " + x->get_spanish_suit(); // i want the setw to apply to this whole string
+		ost << "\t" << left << setw(20) << line_up << "(" << x->get_english_rank() << " of " << x->get_english_suit() << ")" << endl;
 	}
+
+	return;
 }
 
+/**
+	deletes all cards that have been added
+	used after the end of each round
+*/
 void Player::reset_cards()
 {
 	for (vector<Card*>::iterator itr = myhand.begin(); itr != myhand.end(); ++itr)
 		delete *itr;
-
 	myhand = {};
+
+	return;
+}
+
+/*************************************
+	Functions needed to play the game
+**************************************/
+
+
+/**
+	sets the bet to 0, and "do you want another card (y/n)?" back to y
+	@bet amount of bet to be reset to 0
+	@another_card sets y/n to y
+*/
+void reset_round(int& bet, char& another_card)
+{
+	bet = 0;
+	another_card = 'y';
+}
+
+/**
+	writes into the text file the information about the game
+	@param you the main player
+	@param dealer the dealer against whom the player is playing
+	@param ost the ostream object that would be used to log the files into
+	@bet the amount of bet in that round
+	@round_number the round number
+*/
+void log_file(const Player& you, const Player& dealer, ofstream& ost, const int& bet, int& round_number)
+{
+	ost.open("log_file.txt", ios::app);
+
+	ost << "---------------------------------------------\n\nRound number: " << round_number
+		<< "\tMoney left: $" << you.get_money() + bet << "\nBet: $" << bet
+		<< "\n\nYour cards: \n";
+
+	you.log_cards(ost);
+
+	ost << "Your total: " << you.card_total() << ".\n\n";
+
+	if (dealer.card_total() != 0) // if the player went over 7.5 at the start of the round, then dealer wouldn't have any cards 
+	{
+		ost << "Dealer's cards: \n";
+
+		dealer.log_cards(ost);
+
+		ost << "Dealer's total: " << dealer.card_total() << ".\n";
+	}
+
+	ost.close();
 
 }
 
-/*
-void play_game()
-
+/**
+	the main function that executes the game
+	@param you the main player
+	@param dealer the dealer against whom the player is playing
+*/
+void play_game(Player& you, Player& dealer)
 {
 	int starting_money = 0;
 	int bet = 0;
 	char another_card = 'y';
+	int round_number = 0; 
+	ofstream ost; // used for logging into file; declared here so that it wouldn't create ostream onject repeatedly when log_file function is called
 
-	cout << "Welcome to Siete y Medio!" << endl
+	cout << "Welcome to Siete y Medio!\n" << endl
 		<< "Enter your starting money: ";
 	cin >> starting_money;
+	cout << "\n";
 
-	Player you(starting_money);
-	Player dealer;
+	you.set_money(starting_money);
 
 	do
 	{
 		cout << "You have $" << you.get_money() << ". Enter bet: ";
 		cin >> bet;
-		while (bet > you.get_money())
+		cout << "\n";
+
+		// if bet is higher than amount of money, prompt player for a valid input
+		while (bet > you.get_money() || bet == 0)
 		{
 			cout << "Enter a valid bet: ";
 			cin >> bet;
 		}
 
+		// player adds card to his hands
 		while (another_card == 'y')
 		{
 			cout << "Your cards: " << endl;
 			you.draw_card();
 			you.list_cards();
 
-			cout << "Your total is " << you.card_total() << "." << endl << "Do you want another card (y/n)? ";
-			cin >> another_card;
-			if (another_card == 'n')
-				break;
+			cout << "Your total is " << you.card_total() << ". ";
 
-			if (you.check_lose())
+			if (you.check_lose()) // if player went over 7.5, end the round before dealer draws cards  
 			{
-				cout << "Your total is " << you.card_total() << "You lost the round.";
+				cout << "You lost the round. You lose $" << bet << "." << "\n\n";
 				you.lose_bet(bet);
 				break;
 			}
 
+			cout << "Do you want another card (y/n)? ";
+			cin >> another_card;
+			cout << "\n";
+
+			if (another_card == 'n') // player doesn't want another card
+				break;
 		}
 
-
-		while (!you.check_lose())
+		while (!you.check_lose() && dealer.card_total() <= you.card_total()) // if the player hasn't gone over 7.5 yet and
+																			 // the dealer's card hasn't gone over the player's total,
+																			 // then draw more cards
 		{
-			while (dealer.card_total() < you.card_total())
-			{
-				cout << "Dealer's cards:" << endl;
-				dealer.draw_card();
-				dealer.list_cards();
-				cout << "The dealer's total is " << dealer.card_total() << endl;
+			cout << "Dealer's cards:" << endl;
+			dealer.draw_card();
+			dealer.list_cards();
+			cout << "The dealer's total is " << dealer.card_total() << ".\n\n";
 
-				if (dealer.check_lose())
-				{
-					cout << "Congratulations! You won $" << bet << "." << endl;
-					you.win_bet(bet);
-					break;
-				}
-				
-				else if (you.card_total() < dealer.card_total())
-				{
-					cout << "You lost the round. You lose $" << bet << "." << endl;
-					break;
-				}
+			if (dealer.check_lose())
+			{
+				cout << "Congratulations! You won $" << bet << "." << "\n\n";
+				you.win_bet(bet);
+				break;
 			}
+
+			else if (you.card_total() < dealer.card_total())
+			{
+				cout << "You lost the round. You lose $" << bet << "." << "\n\n";
+				you.lose_bet(bet);
+				break;
+			}
+
+			else if (you.card_total() == dealer.card_total())
+			{
+				cout << "It's a tie!\n\n";
+				break;
+			}
+
+			cout << "The dealer is drawing another card...\n\n";
 		}
 
+		if (you.check_end())
+		{
+			cout << "You are out of money. Goodbye!\n\n";
+		}
 
-	} while (you.check_end());
+		round_number++;
+		log_file(you, dealer, ost, bet, round_number);
+
+		reset_round(bet, another_card);
+		you.reset_cards(); // reset rounds 
+		dealer.reset_cards();
+
+	} while (!you.check_end());
+
+
 }
-*/
--- homework1/homework1/Cards.h
+++ homework1/homework1/Cards.h
@@ -30,7 +30,6 @@ public:
 	string get_spanish_rank() const;
 
 	// Accessors (returns suit and rank in English)
-
 	string get_english_suit() const;
 	string get_english_rank() const;
 
@@ -50,34 +49,125 @@ private:
 	rank_t rank;
 };
 
-//consolidate Hand class into Player because it only contains a vector of Card* as a private member 
+//consolidated Hand class into Player because it only contains a vector of Card* as a private member 
 
 class Player 
 {
 public:
 	// default constructor
 	Player();
-	//constructor, assigns initial value of money
+
+	/*
+		constructor, assigns initial value of money
+		@param m initial amount of money
+	*/
 	Player(int m);
+
+	/**
+		computes the total of the value of the cards 
+		@return value of the card
+	*/
 	double card_total() const;
+
+	/**
+		adds another card into the hand by pushing back a Card*
+	*/
 	void draw_card();
+
+	/**
+		deletes all cards that have been added
+		used after the end of each round
+	*/
 	void reset_cards();
-	bool check_lose() const; // check if lost round (ie. card total is more than 7.5)
-	bool check_end() const; // check if end whole game
-	bool operator < (Player another) const; // compare who won between two players based on value of cards
+
+	/**
+		checks if player loses the round (ie. card total is more than 7.5)
+		@return true if lost, false if not
+	*/
+	bool check_lose() const;
+
+	/**
+		checks if the game has reached its end (ie. money == 0)
+		@return true if game is finished, false if not
+	*/
+	bool check_end() const;
+
+	/**
+		compares who won between the two players based on the total value of their cards
+		@param another the player with whom to compare
+		@return true if rhs player has a total higher than the lhs player but lower than 7.5
+	*/
+	bool operator < (const Player& another) const;
+
+	/**
+		accessor for money
+		@return the value of money the player has currently
+	*/
 	int get_money() const;
-	void win_bet(int a);
-	void lose_bet(int b);
+
+	/**
+		sets money of the player
+		@param money to be set
+	*/
+	void set_money(const int& a);
+
+	/**
+		adds bet to the money
+		@param a amount of bet won
+	*/
+	void win_bet(const int& a);
+
+	/**
+		subtracts bet from money
+		@param b amount of money lost
+	*/
+	void lose_bet(const int& b);
+
+	/**
+		list cards into the console using cout
+	*/
 	void list_cards() const;
+
+	/**
+		list cards into an ostream object to be used when logging into a txt file
+		@param ost ostream object to write into the txt file
+	*/
 	void log_cards(ostream& ost) const;
 	
 
 private:
+	
+	//player's money, set by constructors
 	int money;
+
+	//cards by the player 
 	vector<Card*> myhand;
 };
 
+//functions needed to play the game
 
+/**
+	sets the bet to 0, and "do you want another card (y/n)?" back to y
+	@bet amount of bet to be reset to 0
+	@another_card sets y/n to y
+*/
+void reset_round(int& bet, char& another_card);
 
+/**
+	writes into the text file the information about the game
+	@param you the main player
+	@param dealer the dealer against whom the player is playing 
+	@param ost the ostream object that would be used to log the files into
+	@bet the amount of bet in that round
+	@round_number the round number 
+*/
+void log_file(const Player& you, const Player& dealer, ofstream& ost, const int& bet, int& round_number);
+
+/**
+	the main function that executes the game
+	@param you the main player
+	@param dealer the dealer against whom the player is playing
+*/
+void play_game(Player& you, Player& dealer);
 
 #endif
